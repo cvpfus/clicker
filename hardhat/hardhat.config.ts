3aecb88a01693017741f330c44da17c13880fd93
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2025-01-23 20:33:54 +0700

    chore: use 2 testnets for testing

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,58 +1,7 @@
-// import { HardhatUserConfig, vars } from "hardhat/config";
-// import "@nomicfoundation/hardhat-toolbox";
-
-// const TEA_TESTNET_PRIVATE_KEY = vars.get("TEA_TESTNET_PRIVATE_KEY");
-
-// const config: HardhatUserConfig = {
-//   solidity: {
-//     version: "0.8.28",
-//     settings: {
-//       viaIR: true,
-//       optimizer: {
-//         enabled: true,
-//         details: {
-//           yulDetails: {
-//             optimizerSteps: "u",
-//           },
-//         },
-//       },
-//     },
-//   },
-//   networks: {
-//     teaTestnet: {
-//       url: "https://assam-rpc.tea.xyz",
-//       accounts: [TEA_TESTNET_PRIVATE_KEY],
-//     },
-//   },
-//   etherscan: {
-//     apiKey: {
-//       teaTestnet: "just-random-api-key",
-//     },
-//     customChains: [
-//       {
-//         network: "teaTestnet",
-//         chainId: 93384,
-//         urls: {
-//           apiURL: "https://assam.tea.xyz/api",
-//           browserURL: "https://assam.tea.xyz",
-//         },
-//       },
-//     ],
-//   },
-//   sourcify: {
-//     enabled: false,
-//   },
-// };
-
-// export default config;
-
-
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
-const TESTNET_PRIVATE_KEY = vars.get("TEA_TESTNET_PRIVATE_KEY");
-const BSCSCAN_API_KEY = vars.get("BSCSCAN_API_KEY");
+const TEA_TESTNET_PRIVATE_KEY = vars.get("TEA_TESTNET_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -70,15 +19,25 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    bscTestnet: {
-      url: `https://bnb-testnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [TESTNET_PRIVATE_KEY],
+    teaTestnet: {
+      url: "https://assam-rpc.tea.xyz",
+      accounts: [TEA_TESTNET_PRIVATE_KEY],
     },
   },
   etherscan: {
     apiKey: {
-      bscTestnet: BSCSCAN_API_KEY,
+      teaTestnet: "just-random-api-key",
     },
+    customChains: [
+      {
+        network: "teaTestnet",
+        chainId: 93384,
+        urls: {
+          apiURL: "https://assam.tea.xyz/api",
+          browserURL: "https://assam.tea.xyz",
+        },
+      },
+    ],
   },
   sourcify: {
     enabled: false,
@@ -86,3 +45,44 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+
+
+// import { HardhatUserConfig, vars } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+
+// const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+// const TESTNET_PRIVATE_KEY = vars.get("TEA_TESTNET_PRIVATE_KEY");
+// const BSCSCAN_API_KEY = vars.get("BSCSCAN_API_KEY");
+
+// const config: HardhatUserConfig = {
+//   solidity: {
+//     version: "0.8.28",
+//     settings: {
+//       viaIR: true,
+//       optimizer: {
+//         enabled: true,
+//         details: {
+//           yulDetails: {
+//             optimizerSteps: "u",
+//           },
+//         },
+//       },
+//     },
+//   },
+//   networks: {
+//     bscTestnet: {
+//       url: `https://bnb-testnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+//       accounts: [TESTNET_PRIVATE_KEY],
+//     },
+//   },
+//   etherscan: {
+//     apiKey: {
+//       bscTestnet: BSCSCAN_API_KEY,
+//     },
+//   },
+//   sourcify: {
+//     enabled: false,
+//   },
+// };
+
+// export default config;
